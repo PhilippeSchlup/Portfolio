9bdba237ed842593a21adb4e62b57b171b380707
Philippe Schlup <philippe.schlup@gmail.com>
2024-12-17 16:54:50 +0000

    update cv

--- js/download.js
+++ js/download.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('downloadCV');
     button.addEventListener('click', () => {
-        const pdfUrl = 'pdf/cv.pdf'; 
+        const pdfUrl = 'pdf/CV_Philippe_Schlup_en.pdf'; 
         const link = document.createElement('a');
         link.href = pdfUrl;
         link.download = 'pdf/CV_Philippe_Schlup_en.pdf';
